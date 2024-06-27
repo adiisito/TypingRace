2ebf194e66c6901d0deb6ff5186c512970a13d53
Yililalo <Yil.Li@campus.lmu.de>
2024-06-27 19:20:45 +0200

    Handled the other notis on Server

--- src/main/java/communication/messages/EndGameRequest.java
+++ src/main/java/communication/messages/EndGameRequest.java
@@ -5,9 +5,14 @@ public class EndGameRequest{
     private final String messageType = "EndGameRequest";
 
     private String playerName;
+    private int time;
+    private int wpm;
 
-    public EndGameRequest(String playerName) {
+
+    public EndGameRequest(String playerName, int time, int wpm) {
         this.playerName = playerName;
+        this.time = time;
+        this.wpm = wpm;
     }
 
     public String getMessageType() {
@@ -20,5 +25,21 @@ public class EndGameRequest{
 
     public void setPlayerName(String playerName) {
         this.playerName = playerName;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public int getWpm() {
+        return wpm;
+    }
+
+    public void setWpm(int wpm) {
+        this.wpm = wpm;
     }
 }
