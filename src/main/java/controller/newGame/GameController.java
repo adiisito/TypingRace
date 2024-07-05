9c57bf996f127c8a168b2036040cae9867800a26
doganu <doganu@cip.ifi.lmu.de>
2024-07-05 14:58:50 +0200

    mcv

---
+++ src/main/java/controller/newGame/GameController.java
@@ -0,0 +1,12 @@
+package controller.newGame;
+
+import game.Game;
+import view.GUI;
+
+public class GameController {
+
+    private GUI View;
+    private Game newGame;
+
+
+}
---
+++ src/main/java/controller/newGame/typerace.java
@@ -0,0 +1,4 @@
+package controller.newGame;
+
+public class typerace {
+}
