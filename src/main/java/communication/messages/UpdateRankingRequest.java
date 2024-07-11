326ddf8370719310ab4e15382ba5c752c3eb878d
Aditya Bharadwaj <bharadwaj.aditya@campus.lmu.de>
2024-07-11 15:25:49 +0200

    merge from dev

--- src/main/java/communication/messages/UpdateRankingRequest.java
+++ src/main/java/communication/messages/UpdateRankingRequest.java
@@ -1,4 +1,25 @@
 package communication.messages;
 
+import game.Player;
+import game.TypingPlayer;
+
+import java.util.List;
+
 public class UpdateRankingRequest {
+
+    private final String messageType = "UpdateRankingRequest";
+
+    private List<TypingPlayer> players;
+
+    public UpdateRankingRequest(List<TypingPlayer> players) {
+        this.players = players;
+    }
+
+    public List<TypingPlayer> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<TypingPlayer> players) {
+        this.players = players;
+    }
 }
