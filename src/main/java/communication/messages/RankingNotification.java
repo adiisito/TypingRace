2af93b1d72a95f8701f66db5550e2147f3fca7b6
Aditya Bharadwaj <bharadwaj.aditya@campus.lmu.de>
2024-07-11 15:27:24 +0200

    merge from dev

--- src/main/java/communication/messages/RankingNotification.java
+++ src/main/java/communication/messages/RankingNotification.java
@@ -1,4 +1,25 @@
 package communication.messages;
 
+import game.Player;
+import game.TypingPlayer;
+
+import java.util.List;
+
 public class RankingNotification {
+
+    private final String messageType = "RankingNotification";
+
+    private List<TypingPlayer> rankings;
+
+    public RankingNotification(List<TypingPlayer> rankings) {
+        this.rankings = rankings;
+    }
+
+    public List<TypingPlayer> getRankings() {
+        return rankings;
+    }
+
+    public void setRankings(List<TypingPlayer> rankings) {
+        this.rankings = rankings;
+    }
 }
