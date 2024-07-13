b30a1eb031d5fffa2301d09835fecc961335b4f6
Yuanyuan Qu <quy@cip.ifi.lmu.de>
2024-07-13 12:44:10 +0200

    client test

--- src/test/java/network/server/ServerTest.java
+++ src/test/java/network/server/ServerTest.java
@@ -12,27 +12,5 @@ import controller.server.GameServer;
 import network.MockSocket;
 
 public class ServerTest {
-    private GameServer server;
-    private MockSocket mockSocket;
-    private ByteArrayInputStream mockInput;
-    private ByteArrayOutputStream mockOutput;
 
-    @BeforeEach
-    public void setup() throws IOException {
-        mockInput = new ByteArrayInputStream("Messages from Client".getBytes());
-        mockOutput = new ByteArrayOutputStream();
-        mockSocket = new MockSocket(mockInput, mockOutput);
-        server = new GameServer(); // 假设这是处理连接的服务器
-    }
-
-    @Test
-    public void testHandlesJoinRequest() throws IOException {
-        String joinRequest = "{\"type\":\"JoinGame\",\"playerName\":\"Alice\"}";
-        //mockInput.reset(handleJoinGameRequest.getBytes());
-
-        //server.handleJoinGameRequest();
-
-        String response = new String(mockOutput.toByteArray());
-        assertTrue(response.contains("Welcome Alice"));
-    }
 }
